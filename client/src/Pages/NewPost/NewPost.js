254c835b83dd03d2cd0ecbab7a310af193a6fde6
Im-Rudra <mahmudulislam.5995@gmail.com>
2022-02-25 03:59:57 +0600

    jodit rich text editor added

--- client/src/Pages/NewPost/NewPost.js
+++ client/src/Pages/NewPost/NewPost.js
@@ -1,53 +1,40 @@
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
+import './Jodit.min.css';
 import JoditEditor from 'jodit-react';
 import parse from 'html-react-parser';
 
 const NewPost = () => {
   const editor = useRef(null);
   const [editorValue, setEditorValue] = useState('');
-  const editorConfig = {
-    buttons: [
-      'bold',
-      'italic',
-      'underline',
-      'strikethrough',
-      'eraser',
-      'ul',
-      'ol',
-      'font',
-      'fontsize',
-      'paragraph',
-      'classSpan',
-      'lineHeight',
-      'superscript',
-      'subscript',
-      'image',
-      'file',
-      'copyformat',
-      'cut',
-      'copy'
-    ]
+  const config = {
+    uploader: { insertImageAsBase64URI: true },
+    removeButtons: ['about']
   };
 
-  const config = {
-    autofocus: true,
-    uploader: {
-      insertImageAsBase64URI: true
-    },
-    buttons:
-      'bold,italic,underline,strikethrough,eraser,ul,ol,font,fontsize,paragraph,classSpan,lineHeight,superscript,subscript,image,video,file,copyformat,cut'
-  };
-  console.log(editorValue);
   return (
     <div className="container mx-auto">
       <h1 className="text-center text-3xl my-8">Add New Post</h1>
-      <JoditEditor
-        ref={editor}
-        config={config}
-        value={editorValue}
-        onChange={(content) => setEditorValue(content)}
-      />
+      <div className="my-8">
+        <h1 className="text-3xl mb-4">Title</h1>
+        <input
+          className="w-full bg-gray-50 border-0 outline-none rounded-sm focus:ring-1 px-2 py-1 text-2xl"
+          type="text"
+        />
+      </div>
+      {useMemo(
+        () => (
+          <JoditEditor
+            ref={editor}
+            config={config}
+            value={editorValue}
+            tabIndex={1}
+            onChange={(content) => setEditorValue(content)}
+          />
+        ),
+        []
+      )}
       <div>{parse(editorValue)}</div>
+      {/* <div>{editorValue}</div> */}
     </div>
   );
 };
