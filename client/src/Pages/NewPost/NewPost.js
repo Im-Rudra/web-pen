52d9e087d9226ea10097e4f3ad77e77656df65c8
Im-Rudra <mahmudulislam.5995@gmail.com>
2022-02-26 03:50:26 +0600

    graphql-request added

--- client/src/Pages/NewPost/NewPost.js
+++ client/src/Pages/NewPost/NewPost.js
@@ -1,25 +1,65 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './Jodit.min.css';
 import JoditEditor from 'jodit-react';
 import parse from 'html-react-parser';
+import { gql } from 'graphql-request';
+import { request } from 'graphql-request';
 
 const NewPost = () => {
   const editor = useRef(null);
   const [editorValue, setEditorValue] = useState('');
+  const [categories, setCategories] = useState([]);
   const config = {
     uploader: { insertImageAsBase64URI: true },
-    removeButtons: ['about']
+    removeButtons: ['about', 'print'],
+    minHeight: 400,
+    enter: 'DIV'
   };
+  const categoryQuery = gql`
+    {
+      categories {
+        id
+        categoryName
+        slug
+      }
+    }
+  `;
+  useEffect(() => {
+    const fetchData = async () => {
+      const endpoint = 'http://localhost:5000/graphql';
+      const res = await request(endpoint, categoryQuery);
+      setCategories(res?.categories);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-center text-3xl my-8">Add New Post</h1>
+    <div className="container px-2 mx-auto">
+      <h1 className="text-center text-2xl md:text-3xl my-2 md:my-8">
+        Add New Post
+      </h1>
       <div className="my-8">
-        <h1 className="text-3xl mb-4">Title</h1>
+        <h1 className="text-3xl mb-2 md:mb-4">Title</h1>
         <input
-          className="w-full bg-gray-50 border-0 outline-none rounded-sm focus:ring-1 px-2 py-1 text-2xl"
+          className="w-full mb-4 bg-slate-100 focus:bg-slate-50 border-0 outline-none rounded-sm focus:ring-2 px-2 py-1 text-xl md:text-2xl"
           type="text"
+          placeholder="Title Here"
         />
+        <select
+          name="cars"
+          id="cars"
+          className="bg-slate-100 px-2 py-1 focus:ring-2 border-0 outline-none rounded text-xl md:text-2xl"
+          defaultValue=""
+        >
+          <option disabled value="">
+            Select Category
+          </option>
+          {categories?.map((category) => (
+            <option key={category?.id} value={category?.id}>
+              {category?.categoryName}
+            </option>
+          ))}
+        </select>
       </div>
       {useMemo(
         () => (
@@ -34,7 +74,7 @@ const NewPost = () => {
         []
       )}
       <div>{parse(editorValue)}</div>
-      {/* <div>{editorValue}</div> */}
+      <div>{editorValue}</div>
     </div>
   );
 };
